6bc21da88d3df827222437854fc2b621b15645f2
isekaishy-jpg <isekaishy@gmail.com>
2026-09-03 22:39:33 -0400

    chore(rendering): expose particle bone diagnostics

--- crates/rendering/examples/validate_m2_geometry.rs
+++ crates/rendering/examples/validate_m2_geometry.rs
@@ -281,8 +281,9 @@ fn main() -> Result<(), Box<dyn Error>> {
             })
             .collect::<Vec<_>>();
         println!(
-            "particle={particle_index} id={} emitter_type={} particle_type={} head_or_tail={} blend={} flags={:#010X} priority={} position={:?} animated_position={animated_position:?} atlas={}x{} speed={} gravity={} life={} rate={} area={}x{} scale={:?}/{:?} twinkle={}/{:?} wind={:?} drag={} spin={}+/-{} textures={textures:?}",
+            "particle={particle_index} id={} bone={:?} emitter_type={} particle_type={} head_or_tail={} blend={} flags={:#010X} priority={} position={:?} animated_position={animated_position:?} atlas={}x{} speed={} gravity={} life={} rate={} area={}x{} scale={:?}/{:?} twinkle={}/{:?} wind={:?} drag={} spin={}+/-{} textures={textures:?}",
             particle.id(),
+            particle.bone_index(),
             particle.emitter_type(),
             particle.particle_type(),
             particle.head_or_tail(),
